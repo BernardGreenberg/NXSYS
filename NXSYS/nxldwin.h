c020422c6ce0e3561ee60c96a93f83208bdc5fdd
Bernard Greenberg <53483365+BernardGreenberg@users.noreply.github.com>
2022-02-12 16:04:59 -0500

    RelayShowString from int to void (inconsistent on mac).

--- NXSYS/nxldwin.h
+++ NXSYS/nxldwin.h
@@ -2,7 +2,7 @@
 #define _NXSYS_LOGIC_DRAW_WINDOW_API_H__
 
 BOOL RegisterRelayLogicWindowClass (HINSTANCE hInstance);
-int RelayShowString (const char *rnm);
+void RelayShowString (const char *rnm);
 void AskForAndDrawRelay (HWND hWnd);
 void AskForAndShowStateRelay (HWND hWnd);
 void DraftsbeingCleanupForOneLayout();
