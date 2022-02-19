9ef0736ecde17419e53ce5ee0f25f23dd8eb910a
Bernard Greenberg <53483365+BernardGreenberg@users.noreply.github.com>
2022-02-19 13:05:08 -0500

    Make HTML scenario help work on Windows
    ... and STL-ize helpdlg.cpp.

--- NXSYSWindows/helpdlg.cpp
+++ NXSYSWindows/helpdlg.cpp
@@ -3,19 +3,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 #include "nxsysapp.h"
 #include "helpdlg.h"
 #include "usermsg.h"
 #include "resource.h"
+#include "WinReadResText.h"
 
 /* 11 January 2001 */
+/* thorough rewrite 19 Feb 2022 */
 
 #ifdef NXV2
 #define HELP_SUBMENU_X 5
 #else
 #define HELP_SUBMENU_X 6
 #endif
+using std::string, std::vector;
 
 static WNDPROC wpOrigEditProc;
 extern std::string HelpPath;
@@ -23,75 +27,74 @@ extern std::string HelpPath;
 #define MAX_HELP_TEXTS 10
 
 struct HelpText {
-    const char * Title;
-    const char * Text;
-    UINT FixedId;
-    UINT CmdId;
-    void Setup (const char * text, const char * title, UINT hfid);
+	HelpText(const char* text, const char* title) : Text(text), Title(title) {}
+	HelpText(const char* text, const char* title, UINT hfid);
+    string Title;
+    string Text;
+    int FixedId;
+    int CmdId;
     void Display();
     void Remove(HMENU hHelpMenu);
     void Dialog();
 };
 
-static HelpText HelpTexts[MAX_HELP_TEXTS];
-static int NHelpTexts = 0;
-
+static vector<struct HelpText> HelpTexts;
 
 /* crockamarola to prevent text in edit box from selecting */
-static BOOL APIENTRY
+static UINT_PTR APIENTRY
    EditBoxSubclassProc
    (HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
     LRESULT lv = CallWindowProc (wpOrigEditProc, hwnd, uMsg, wParam, lParam);
     if (uMsg == WM_GETDLGCODE)
-	lv &= ~DLGC_HASSETSEL;
+		lv &= ~DLGC_HASSETSEL;
     return lv;
 }
 
 
 static INT_PTR CALLBACK
-   HelpDlgProc (HWND dialog, unsigned message, WPARAM wParam, LPARAM lParam) {
+HelpDlgProc(HWND dialog, unsigned message, WPARAM wParam, LPARAM lParam) {
 
-    HWND edit;
-    switch (message) {
+	HWND edit;
+	switch (message) {
 	case WM_INITDIALOG:
 	{
-	    HelpText * pHT = (HelpText*)lParam;
-	    SetWindowText (dialog, pHT->Title);
-	    edit = GetDlgItem(dialog, IDC_HELPDLG_TEXT);
+		HelpText* pHT = (HelpText*)lParam;
+		SetWindowText(dialog, pHT->Title.c_str());
+		edit = GetDlgItem(dialog, IDC_HELPDLG_TEXT);
 
-	    if (edit) {
-		SetWindowText (edit, pHT->Text);
-		wpOrigEditProc
-			= (WNDPROC) SetWindowLongPtr
-			  (edit, GWLP_WNDPROC, (INT_PTR) EditBoxSubclassProc);
-	    }
-	    return TRUE;
+		if (edit) {
+			SetWindowText(edit, pHT->Text.c_str());
+			wpOrigEditProc
+				= (WNDPROC)SetWindowLongPtr
+				(edit, GWLP_WNDPROC, (INT_PTR)EditBoxSubclassProc);
+		}
+		return TRUE;
 	}
 	case WM_COMMAND:
-	    switch (LOWORD(wParam)) {
+		switch (LOWORD(wParam)) {
 		case IDOK:
-		    EndDialog (dialog, 1);
-		    return TRUE;
+			EndDialog(dialog, 1);
+			return TRUE;
 		case IDCANCEL:
-		    EndDialog (dialog, 0);
-		    return TRUE;
+			EndDialog(dialog, 0);
+			return TRUE;
 		default:
-		    return FALSE;
-	    }
-	    return FALSE;
+			return FALSE;
+		}
+		return FALSE;
 
 	case WM_DESTROY:
-	    edit = GetDlgItem(dialog, IDC_HELPDLG_TEXT);
-	    if (edit) {
-		SetWindowLongPtr(edit, GWLP_WNDPROC, (INT_PTR) wpOrigEditProc);
-		return TRUE;
-	    }
-	    return FALSE;
+		edit = GetDlgItem(dialog, IDC_HELPDLG_TEXT);
+		if (edit) {
+			SetWindowLongPtr(edit, GWLP_WNDPROC, (INT_PTR)wpOrigEditProc);
+			return TRUE;
+		}
+		return FALSE;
 
 	default:
-	    return FALSE;
-    }
+		return FALSE;
+	}
 }
 
 void HelpText::Dialog () {
@@ -100,24 +103,19 @@ void HelpText::Dialog () {
 }    
 
 void HelpDialog (const char * text, const char * title) {
-    HelpText HT;
-    HT.Title = title;
-    HT.Text = text;
-    HT.Dialog();
+	HelpText( text, title ).Dialog();
 }
 
-void HelpText::Display () {
-    if (FixedId)
-	WinHelp (G_mainwindow, HelpPath.c_str(), HELP_CONTEXT, FixedId);
-    else
-	Dialog();
+void HelpText::Display() {
+	if (FixedId)
+		WinHelp(G_mainwindow, HelpPath.c_str(), HELP_CONTEXT, FixedId);
+	else if (Text.length() >= 5 && Text.substr(0, 5) == "file:")
+		WinBrowseResource(Text.c_str());
+	else
+		Dialog();
 }
 
 void HelpText::Remove (HMENU hHelpMenu) {
-    if (Title)
-	free((PVOID)Title);
-    if (Text)
-	free((PVOID)Text);
     DeleteMenu (hHelpMenu, CmdId, MF_BYCOMMAND);
 }
 
@@ -131,42 +129,29 @@ static HMENU GetHelpSubMenu () {
     return GetSubMenu (hMenu, HELP_SUBMENU_X);
 }
 
-static bool CheckNotTooManyHelps (LPCSTR title) {
-    if (NHelpTexts >= MAX_HELP_TEXTS) {
-	usermsg ("Maximum number of help menu texts (%d) exceeded. Can't"
-		 " put text \"%s\" in help menu.",
-		 MAX_HELP_TEXTS, title);
-	return false;
-    }
-    return true;
-}
-
-void HelpText::Setup (const char * text, const char * title, UINT hfid) {
-    Text = text ? _strdup(text) : NULL;
-    Title = _strdup(title);
+HelpText::HelpText (const char * text, const char * title, UINT hfid) {
+	if (text) Text = text;
+	if (title) Title = title;
     FixedId = hfid;
-    CmdId = ID_EXTHELP0 + this - HelpTexts;
+	CmdId = UINT(int(ID_EXTHELP0) + (int)HelpTexts.size());
     HMENU hHelpMenu = GetHelpSubMenu();
     if (CmdId == ID_EXTHELP0) //Good idea, Henry
-	AppendMenu (hHelpMenu, MF_SEPARATOR, NULL, 0);
-    AppendMenu (hHelpMenu, MF_STRING | MF_ENABLED, CmdId, Title);
+		AppendMenu (hHelpMenu, MF_SEPARATOR, NULL, 0);
+    AppendMenu (hHelpMenu, MF_STRING | MF_ENABLED, CmdId, Title.c_str());
 }    
 
 void RegisterHelpMenuText (const char * text, const char * title) {
-    if (CheckNotTooManyHelps(title))
-	HelpTexts[NHelpTexts++].Setup (text, title, 0);
+	HelpTexts.emplace_back (text, title, 0);
 }
 
 void RegisterInHelpfileMenuItem (const char * title, UINT helpfile_id) {
-    if (CheckNotTooManyHelps(title))
-	HelpTexts[NHelpTexts++].Setup (NULL, title, helpfile_id);
+	HelpTexts.emplace_back ("", title, helpfile_id);
 }
 
 void ClearHelpMenu () {
     HMENU hHelpMenu = GetHelpSubMenu();
-    for (int i = 0; i < NHelpTexts; i++)
-	HelpTexts[i].Remove(hHelpMenu);
+	for (auto& help : HelpTexts)	
+		help.Remove(hHelpMenu);
     DeleteMenu (hHelpMenu, 0, MF_SEPARATOR);
-    NHelpTexts = 0;
 }
 
