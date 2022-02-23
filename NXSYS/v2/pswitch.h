0545fbeba0869cca074f98839edf0cf9e36df385
Bernard Greenberg <53483365+BernardGreenberg@users.noreply.github.com>
2022-02-23 14:47:43 -0500

    pswitch.h got away, too (TlDlgProc kludge)

--- NXSYS/v2/pswitch.h
+++ NXSYS/v2/pswitch.h
@@ -31,7 +31,7 @@ class PanelSwitch : public GraphicObject {
     std::string RelayNomenclature;
     virtual void EditClick(int x, int y);
     virtual ~PanelSwitch();
-    virtual BOOL DlgProc (HWND hDlg, UINT msg, WPARAM, LPARAM);
+    virtual BOOL_DLG_PROC_QUAL DlgProc (HWND hDlg, UINT msg, WPARAM, LPARAM);
     virtual int Dump (FILE * f);
 #endif
 };
