0e82c2299bada79390f944c1cc6bcf31551b1d52
Bernard Greenberg <53483365+BernardGreenberg@users.noreply.github.com>
2022-04-09 19:44:15 -0400

    Redo TLEdit Objreg in STL

--- TLEdit/tled/objreg.cpp
+++ TLEdit/tled/objreg.cpp
@@ -1,63 +1,41 @@
-#define OBJ_TYPE_REGISTRY_MAX 50
-
 #include "windows.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdarg.h>
+#include <unordered_map>
 
 #include "compat32.h"
 #include "nxgo.h"
-#include "xtgtrack.h"
 #include "tledit.h"
 #include "objreg.h"
 
+/* Rewritten for dyn-create STL 4/9/2022 */
+
 struct NXObjectRegistryEntry {
-    short ObjectId;
-    short DialogId;
-    int Command;
     ObjCreateFn ObjectCreationFunction;
     ObjClassInitFn ObjectClassInitFunction;
-    int ObjectCount;
 };
 
-
-static int N_Registry = 0;
-static NXObjectRegistryEntry Registry[OBJ_TYPE_REGISTRY_MAX];
-
-typedef NXObjectRegistryEntry *ObjRegEntP;
+static std::unordered_map<int, UINT> *DlgIdByObjid = nullptr;
+static std::unordered_map<int, NXObjectRegistryEntry> *FnsByCommand = nullptr;
 
 NXObTypeRegistrar::NXObTypeRegistrar
    (int objid, int command, UINT dlg_id, ObjCreateFn ocf, ObjClassInitFn ocif) {
-    /* check overflow */
-    ObjRegEntP e = &Registry[N_Registry++];
-    e->ObjectId = objid;
-    e->Command = command;
-    e->DialogId = dlg_id;
-    e->ObjectCreationFunction = ocf;
-    e->ObjectClassInitFunction = ocif;
-    e->ObjectCount = 0;
+    /* "load order fiasco" "pattern" -- can't use static maps */
+    if (FnsByCommand == nullptr) {
+        DlgIdByObjid = new std::unordered_map<int, UINT>;
+        FnsByCommand= new std::unordered_map<int, NXObjectRegistryEntry>;
+    }
+
+    (*FnsByCommand)[command] = NXObjectRegistryEntry{ocf, ocif};
+    (*DlgIdByObjid)[objid] = dlg_id;
 }
 
 UINT FindDialogIdFromObjClassRegistry (int objid) {
-    for (int i = 0; i < N_Registry; i++) {
-	ObjRegEntP e = &Registry[i];
-	if (e->ObjectId == objid)
-	    return e->DialogId;
-    }
-    return 0;
+    return (*DlgIdByObjid).count(objid) ? (*DlgIdByObjid)[objid] : 0;
 }
 
 GraphicObject * CreateObjectFromCommand (HWND hWnd, int command, int x, int y) {
-    int i;
-    ObjRegEntP e = NULL;
-    for (i = 0; i < N_Registry; i++) {
-	e = &Registry[i];
-	if (e->Command == command)
-	    break;
-    }
-    if (i >= N_Registry)
-	return NULL;
+    if ((*FnsByCommand).count(command) == 0)
+        return NULL;
+
     RECT r;
     GetClientRect (hWnd, &r);
     if (x < r.left) x = r.left;
@@ -66,13 +44,13 @@ GraphicObject * CreateObjectFromCommand (HWND hWnd, int command, int x, int y) {
     if (y > r.bottom) y = r.bottom;
     int wpx = (int)(SCXtoWP (x) /* + Xoff*/);
     int wpy = (int)(SCYtoWP (y) /* + Yoff*/);
-    return e->ObjectCreationFunction(wpx, wpy);
+    return (*FnsByCommand)[command].ObjectCreationFunction(wpx, wpy);
 }
 
 void InitializeRegisteredObjectClasses() {
-    for (int i = 0; i < N_Registry; i++) {
-	ObjRegEntP e = &Registry[i];
-	if (e->ObjectClassInitFunction != NULL)
-	    (e->ObjectClassInitFunction)();
+    for (auto& pair : *FnsByCommand) {
+        auto E = pair.second;
+        if (E.ObjectClassInitFunction != NULL)
+            E.ObjectClassInitFunction();
     }
 }
