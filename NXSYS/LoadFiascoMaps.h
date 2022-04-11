e5d0f1cdc57c3e191742ae3066bc91fac2fd327d
Bernard Greenberg <53483365+BernardGreenberg@users.noreply.github.com>
2022-04-11 11:40:14 -0400

    Terrific full-explanation comment in LoadFiascoMaps.h

--- NXSYS/LoadFiascoMaps.h
+++ NXSYS/LoadFiascoMaps.h
@@ -11,46 +11,87 @@
 
 #include <unordered_map>
 #include <map>
+/*  Use case:
+ 
+ Assume files A and B contain the following.  "Register" is a function that is
+ called by load-time code in many files to register objects defined in them. The
+ assumption is that there are many files like A, registering their own objects.
+ This architecture is far better, having diverse files do this independently and
+ anonymously rather than that the central registrar be required to know about how
+ to call into each of them separately:
+ 
+ File A:
+     class foo : public generic_object { ... ... };
+     generic_object* foo_fun () { return new foo(...);}
+     static int dummy_var = Register("foo", foo_fun);
+ File B:
+     std::map(std::string, generic_object*(*)void)) creators;
+     void Register (const char * key, generic_object*(*function)(void))) {
+          creators[key] = function;
+     }
 
-/* Instances of these wrappers on maps can be used as static maps that can be added to
- by functions in files whose static-var initializers have not been run, and they will function
- whether called before or after those initialilzations, beating the "load order fiasco"
- at least for STL maps ...*/
+If A appears before B in the load order, this will not work: The load-time code in A calling
+"Register" will run before the load-time code in B initializing the std::map has been run,
+and thus, Register will crash.  This is called the "C load order fiasco". Load order is not
+really controllable in some IDE's, and ought not, or can't, be customized for single (possibly
+conflicting) problems.  If, instead of std::map, LoadFiascoProtectedMap defined here is used,
+the above code will work flawlessly.
+ 
+It relies on a fairly recent enhancement to C++ that initializes elements of statically-
+allocated structures declared with "inline" initializers (int foo = 3;) not at the time
+that the module's static initializers are run, but at the much earlier time that the
+module is first loaded and "simple" static variables are initialized and contained
+functions defined.  This works IF AND ONLY IF the structures DO NOT CONTAIN ANY ELEMENTS
+REQUIRING CODE TO INITIALIZE; as can be proven below, if ANY elements do require code
+to be run, the structure is re-initialized when the load-time code of the containing module
+is executed, corrupting content (e.g., emptying the map by virtue of the pointer being reset
+to nullptr).  By virtue of the very nature of the problem, it is not even possible to set a
+switch to assert that this has not happened!
+
+ */
 
 template<class keytype, class datumtype, class maptype>
 class LoadFiascoProtectedGeneralMap {
+    
     /* As long as there is no initialization code that needs to be run to initialize
-     instances of this, just POD, it all seems to work (on mac at least). Can't yet
-     find language refs. */
+     instances of this, just POD, it all seems to work (Mac clamg & Windows VC). Can't yet
+     find appropriate language refs. */
+
+    /* The null or non-null status of this variable determines whether or not a map
+     need be created */
 
     maptype *map_ptr = nullptr;
-    int x = 72;
+
     void ensure_map() {
         if (map_ptr == nullptr)
             map_ptr = new maptype;
-/* Turn this on and x increases from 72 every time (for each instance), before and after
+
+/* Turn this on and counter increases from 72 every time (for each instance), before and after
    presumed load-initialization of the containing module. Initialization of POD members
    is not an accident.*/
 
 #ifdef FIASCO_PRINT_TRACE
-        printf("%3ld %p %p %d\n", map_ptr->size(), this, map_ptr, x++);
+        printf("%3ld %p %p %d\n", map_ptr->size(), this, map_ptr, counter++);
 #endif
     }
 
-/*   But add enable this code, and the map_ptr will be reinitialized when
-    the containing module's load-time initializations run, destroying the data and
-    recreating the "load order fiasco". This only works when there is no non-POD
-    initialization code in the class. Do not mess with this code! */
+/*   But add enable this code, (change ifdef to ifndef) and the map_ptr will be
+    reinitialized when the containing module's load-time initializations run, destroying
+    the data and recreating the "load order fiasco". This hack only works when there is no
+    non-POD initialization code in the class. Do not mess with this code! */
+
+    int counter = 72;  // for debugging; by hypothesis, requiring zero load-time cycles.
 
 #ifdef CAUSE_FIASCO_ANEW
-    int z() {
-        printf("z %p\n", this);
+    int side_effector() {
+        printf("side_effector %p counter %d\n", this, counter);
         return 3;
     }
-    int y = z();
+    int dumy1 = side_effector();
 #endif
 
 public:
+    /* Add more as needed (e.g., emplace(es)).  These cover all the cases I need. */
     ~LoadFiascoProtectedGeneralMap() {
         if (map_ptr)
             delete map_ptr;
@@ -83,6 +124,12 @@ public:
     }
 };
 
+/* Actual template classes for use.  Although std::unordered_map is better in almost all
+   cases, it does not work with Cocoa NSStrings as key, but std::map does.
+
+   A similar std::vector wrapper ("just accumulate, no map") is imaginable, but not needed yet.
+ 
+ */
 
 template <class keytype, class datumtype>
 class LoadFiascoProtectedUnorderedMap : public
